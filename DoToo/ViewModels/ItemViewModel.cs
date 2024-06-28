982785a1faee0d1af79d7d80d53f22924c0243dc
Moises Croes <mcg-360@hotmail.com>
2024-06-28 03:10:23 +0200

    Add view models by domain entities

---
+++ DoToo/ViewModels/ItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DoToo.Repositories;
+using DoToo.Repositories.Contracts;
+
+namespace DoToo.ViewModels;
+
+public class ItemViewModel : ViewModel
+{
+    private readonly ITodoItemRepository repository;
+    public ItemViewModel(ITodoItemRepository repository) 
+        => this.repository = repository;
+}
---
+++ DoToo/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DoToo.Repositories.Contracts;
+
+namespace DoToo.ViewModels;
+public class MainViewModel : ViewModel
+{
+    private readonly ITodoItemRepository repository;
+    public MainViewModel(ITodoItemRepository repository)
+    {
+        this.repository = repository;
+        Task.Run(LoadDataAsync);
+    }
+    private async Task LoadDataAsync()
+    {
+    }
+}
