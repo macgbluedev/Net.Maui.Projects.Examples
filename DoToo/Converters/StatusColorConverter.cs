d263bba0b81be07f34c60d0532fe6ae4815b9d73
Moises Croes <mcg-360@hotmail.com>
2024-07-06 12:56:30 +0200

    Add status color converter

---
+++ DoToo/Converters/StatusColorConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace DoToo.Converters;
+public class StatusColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+        => (Color)Application.Current.Resources[(bool)value ? "CompletedColor" : "ActiveColor"];
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+        => null;
+}
